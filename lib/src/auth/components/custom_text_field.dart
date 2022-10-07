2aa62c7d486ff7e62755006c491d6c6f78c186a7
Weverton Couto <wevertoncristais@gmail.com>
2022-10-06 22:55:07 -0300

    add sign in screen

---
+++ lib/src/auth/components/custom_text_field.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final IconData iconData;
+  final String label;
+  final bool isSecret;
+
+  const CustomTextField(
+      {required this.iconData,
+      required this.label,
+      this.isSecret = false,
+      super.key});
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool isObscure = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isObscure = widget.isSecret;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: TextFormField(
+        obscureText: isObscure,
+        decoration: InputDecoration(
+            suffixIcon: widget.isSecret
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isObscure = !isObscure;
+                      });
+                    },
+                    icon: Icon(
+                        isObscure ? Icons.visibility : Icons.visibility_off))
+                : null,
+            prefixIcon: Icon(widget.iconData),
+            labelText: widget.label,
+            isDense: true,
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(18))),
+      ),
+    );
+  }
+}
---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,150 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:green_grocer/src/auth/components/custom_text_field.dart';
+import 'package:green_grocer/src/config/custom_colors.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              // app name and category
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // app name
+                    Text.rich(TextSpan(
+                        style: const TextStyle(fontSize: 40),
+                        children: [
+                          const TextSpan(
+                              text: 'Green',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                          TextSpan(
+                              text: 'grocer',
+                              style: TextStyle(
+                                  color: CustomColors.customContrastColor))
+                        ])),
+                    // category
+                    SizedBox(
+                      height: 30,
+                      child: DefaultTextStyle(
+                        style: const TextStyle(fontSize: 25),
+                        child: AnimatedTextKit(
+                            pause: Duration.zero,
+                            repeatForever: true,
+                            animatedTexts: [
+                              FadeAnimatedText('Frutas'),
+                              FadeAnimatedText('Verduras'),
+                              FadeAnimatedText('Legumes'),
+                              FadeAnimatedText('Carnes'),
+                            ]),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Form
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(45))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    //Email text
+                    const CustomTextField(
+                        iconData: Icons.email, label: 'Email'),
+                    //Password text
+                    const CustomTextField(
+                        iconData: Icons.password,
+                        label: 'Password',
+                        isSecret: true),
+                    // Login button
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18))),
+                          onPressed: () {},
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(fontSize: 18),
+                          )),
+                    ),
+                    // forgot password buttom
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Esqueceu a senha?',
+                          style: TextStyle(
+                              color: CustomColors.customContrastColor),
+                        ),
+                      ),
+                    ),
+                    // Divider
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey.withAlpha(90),
+                              thickness: 2,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            child: Text('Ou'),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey.withAlpha(90),
+                              thickness: 2,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // new user button
+                    SizedBox(
+                      height: 50,
+                      child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18)),
+                              side: const BorderSide(
+                                  width: 2, color: Colors.green)),
+                          onPressed: () {},
+                          child: const Text(
+                            'Criar conta',
+                            style: TextStyle(fontSize: 18),
+                          )),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
