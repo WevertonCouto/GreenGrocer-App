dc1406b262e7774aea832e856ed146a194c57c4c
Weverton Couto <wevertoncristais@gmail.com>
2022-10-08 17:56:21 -0300

    add sign up screen

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:green_grocer/src/auth/components/custom_text_field.dart';
+import 'package:green_grocer/src/auth/sign_up_screen.dart';
 import 'package:green_grocer/src/config/custom_colors.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -72,7 +73,7 @@ class SignInScreen extends StatelessWidget {
                     //Password text
                     const CustomTextField(
                         iconData: Icons.password,
-                        label: 'Password',
+                        label: 'Senha',
                         isSecret: true),
                     // Login button
                     SizedBox(
@@ -132,7 +133,10 @@ class SignInScreen extends StatelessWidget {
                                   borderRadius: BorderRadius.circular(18)),
                               side: const BorderSide(
                                   width: 2, color: Colors.green)),
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: ((context) => SignUpScreen())));
+                          },
                           child: const Text(
                             'Criar conta',
                             style: TextStyle(fontSize: 18),
---
+++ lib/src/auth/sign_up_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+import '../config/custom_colors.dart';
+import 'components/custom_text_field.dart';
+
+class SignUpScreen extends StatelessWidget {
+  SignUpScreen({super.key});
+
+  final cpfFormatter = MaskTextInputFormatter(
+      mask: '###.###.###-##', filter: {'#': RegExp(r'[0-9]')});
+
+  final phoneFormatter = MaskTextInputFormatter(
+      mask: '## # ####-####', filter: {'#': RegExp(r'[0-9]')});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  // screen text
+                  const Expanded(
+                    child: Center(
+                      child: Text(
+                        'Cadastro',
+                        style: TextStyle(fontSize: 35, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  // Form
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 32, vertical: 40),
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(45))),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        //Email text
+                        const CustomTextField(
+                            iconData: Icons.email, label: 'Email'),
+                        //Password text
+                        const CustomTextField(
+                            iconData: Icons.password,
+                            label: 'Senha',
+                            isSecret: true),
+                        //Name text
+                        const CustomTextField(
+                            iconData: Icons.person, label: 'Nome'),
+                        //Phone text
+                        CustomTextField(
+                          iconData: Icons.person,
+                          label: 'Celular',
+                          inputFormatters: [phoneFormatter],
+                        ),
+                        //CPF text
+                        CustomTextField(
+                            iconData: Icons.person,
+                            label: 'CPF',
+                            inputFormatters: [cpfFormatter]),
+                        SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(18))),
+                              onPressed: () {},
+                              child: const Text(
+                                'Cadastrar usuário',
+                                style: TextStyle(fontSize: 18),
+                              )),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Positioned(
+                left: 10,
+                top: 10,
+                child: SafeArea(
+                    child: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.white,
+                        ))),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
