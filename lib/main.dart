826774dcd99983f04341a617fcb6b6b4c3c5e6bb
Weverton Couto <wevertoncristais@gmail.com>
2022-10-09 19:23:51 -0300

    add base screen

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       debugShowCheckedModeBanner: false,
-      home: Container(),
+      home: SignInScreen(),
     );
   }
 }
--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:green_grocer/src/auth/components/custom_text_field.dart';
 import 'package:green_grocer/src/auth/sign_up_screen.dart';
+import 'package:green_grocer/src/base/base_screen.dart';
 import 'package:green_grocer/src/config/custom_colors.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -82,7 +83,11 @@ class SignInScreen extends StatelessWidget {
                           style: ElevatedButton.styleFrom(
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(18))),
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => BaseScreen()));
+                          },
                           child: const Text(
                             'Entrar',
                             style: TextStyle(fontSize: 18),
