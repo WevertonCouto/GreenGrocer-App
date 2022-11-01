e83bb6d5acd69262127ceee1fea3b46ea647078a
Weverton Couto <wevertoncristais@gmail.com>
2022-11-01 18:40:33 -0300

    add click animation gesture detector

--- lib/src/pages/home/components/category_tile.dart
+++ lib/src/pages/home/components/category_tile.dart
@@ -14,25 +14,30 @@ class CategoryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Align(
-        alignment: Alignment.center,
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 6),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: isSelected
-                  ? CustomColors.customSwatchColor
-                  : Colors.transparent),
-          child: Text(
-            category,
-            style: TextStyle(
-                color: isSelected
-                    ? Colors.white
-                    : CustomColors.customContrastColor,
-                fontWeight: FontWeight.bold,
-                fontSize: isSelected ? 16 : 14),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onPressed,
+        child: Align(
+          alignment: Alignment.center,
+          child: Ink(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 6),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: isSelected
+                      ? CustomColors.customSwatchColor
+                      : Colors.transparent),
+              child: Text(
+                category,
+                style: TextStyle(
+                    color: isSelected
+                        ? Colors.white
+                        : CustomColors.customContrastColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: isSelected ? 16 : 14),
+              ),
+            ),
           ),
         ),
       ),
